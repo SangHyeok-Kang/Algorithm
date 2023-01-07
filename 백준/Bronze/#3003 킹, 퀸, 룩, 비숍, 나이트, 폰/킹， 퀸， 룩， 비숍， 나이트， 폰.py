3e8845add3379fadfb1e288a6a4383385830692f
SangHyeok-Kang <104892909+SangHyeok-Kang@users.noreply.github.com>
2023-01-07 20:57:00 +0900

    Delete 백준/Bronze/#3003 킹, 퀸, 룩, 비숍, 나이트, 폰 directory

--- 백준/Bronze/#3003 킹, 퀸, 룩, 비숍, 나이트, 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++
@@ -1,8 +0,0 @@
-king, queen, look, bishop, knight, pawn = map(int, input().split())
-tot_king = 1
-tot_queen = 1
-tot_look = 2
-tot_bishop = 2
-tot_knight = 2
-tot_pawn = 8
-print(tot_king - king, tot_queen-queen, tot_look - look, tot_bishop - bishop, tot_knight - knight, tot_pawn-pawn)
