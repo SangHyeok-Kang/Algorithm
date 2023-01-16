a30db472424a6302ffafa918d72300e9909b39e6
SangHyeok-Kang <104892909+SangHyeok-Kang@users.noreply.github.com>
2023-01-17 02:38:57 +0900

    [Bronze III 4690] Title: 완전 세제곱, Time: 132 ms, Memory: 115004 KB -NKLCBHub

--- 백준/Bronze/#4690 완전 세제곱/완전 세제곱.py
+++ 백준/Bronze/#4690 완전 세제곱/완전 세제곱.py
@@ -1,8 +1,11 @@
-temp = [i**3 for i in range(0,101)]
-
-for a in range(2,101):
+for a in range(6,101):
+    tmp = a**3
     for b in range(2, a):
+        tmp2 = b**3
         for c in range(b+1, a):
+            tmp3 = c**3
             for d in range(c+1, a):
-                if temp[a] == temp[b] + temp[c] + temp[d]:
+                tmp4 = d**3
+                if tmp == tmp2 + tmp3 + tmp4:
                     print("Cube = "+ str(a) +", Triple = ("+ str(b) +","+ str(c) +","+ str(d) +")")
+
